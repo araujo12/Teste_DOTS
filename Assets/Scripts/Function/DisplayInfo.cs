79f6c3507a1381214b52f474f2b16c201a8171f9
Araujo <azarujo100@gmail.com>
2023-09-24 17:48:12 -0300

    Modificação nos displays
    Foi alterado os displays das esferas

---
+++ Assets/Scripts/Function/DisplayInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayInfo : MonoBehaviour
+{
+    public SphereST sphereST;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI descriptionText;
+    void Start()
+    {
+        nameText.text = sphereST.nome;
+        descriptionText.text = sphereST.descricao;
+    }
+
+}
